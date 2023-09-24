131bd29f68223a812eed4d78154836ee4efa76cb
MorbidDesire <Ra1jinThunderkeg@yandex.ru>
2023-09-24 20:23:49 +0300

    first step

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ const App = () => (
         target="_blank"
         rel="noopener noreferrer"
       >
-        Learn React and Alex is the Worst!
+        Learn React and Alex is the Worstest!
       </a>
     </header>
   </div>
